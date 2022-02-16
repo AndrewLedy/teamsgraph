c3ea903421fe15777030b83fd346d52566ce2e54
Andrew Subash <subashandrew@gmail.com>
2022-02-16 10:28:50 +0530

    UI changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { delay } from 'rxjs/operator/delay';
 import { UsersService } from './users.service';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
